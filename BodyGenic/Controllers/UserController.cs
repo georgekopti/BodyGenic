3622514dd4595e9b5caca5e11ad4049a96c98f23
Mohit Mamtani <mamtani@sheridancollege.ca>
2020-12-12 19:39:47 -0500

    Connecting Firebase database for login

---
+++ BodyGenic/Controllers/UserController.cs
@@ -0,0 +1,50 @@
+﻿using BodyGenic.Models;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BodyGenic.Controllers
+{
+    public class UserController : Controller
+    {
+        IFirebaseConfig config = new FirebaseConfig
+        {
+            AuthSecret = "FVQVcbV5gkErLtNwsDoJ8jqpfTeIEF2vMzQcV8rw",
+            BasePath = "https://bodygenic-768eb-default-rtdb.firebaseio.com/"
+        };
+        IFirebaseClient client;
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+       
+        public ActionResult Create(User user)
+        {
+            try {
+                AddUserToFirebase(user);
+                ModelState.AddModelError(string.Empty, "Added successfully");
+            }
+            catch(Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, ex.Message);
+            }
+            
+            return View();
+        }
+
+        private void AddUserToFirebase(User user)
+        {
+            client = new FireSharp.FirebaseClient(config);
+            var data = user;
+            PushResponse response = client.Push("Users/", data);
+            data.user_id = response.Result.name;
+            SetResponse setResponse = client.Set("Users/"+data.user_id, data);
+        }
+    }
+}
---
+++ BodyGenic/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BodyGenic.Models
+{
+    public class User
+    {
+        public string user_id { get; set; }
+        public string fullname { get; set; }
+        public string username { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string weight { get; set; }
+        public string height { get; set; }
+        public string dob { get; set; }
+        public string fitness_level { get; set; }
+        public string user_profile_type { get; set; } // client, trainer, nutritionist
+        
+
+    }
+}
