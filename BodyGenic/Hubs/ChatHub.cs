62d78e3c28a8eb154c13eb33325b585b4b2ee2d4
Mohit Mamtani <mamtani@sheridancollege.ca>
2020-12-19 05:53:08 -0500

    Added chat feature

---
+++ BodyGenic/Hubs/ChatHub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BodyGenic.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello(DateTime.Now);
+        }
+        public void Send(string name, string message) 
+        {
+            Clients.All.sendMessageToAllClients(name, message, DateTime.Now);
+        }
+    }
+}
