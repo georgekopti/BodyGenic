7cc1917f59dd9ab0f2635c8e91a4b2711ab551c4
George <koptig@sheridancollege.ca>
2020-12-18 19:50:27 -0500

    Added landing page

--- BodyGenic/Models/viewModel/LandingPage.cs
+++ BodyGenic/Models/viewModel/LandingPage.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using BodyGenic.Models;
 
 namespace BodyGenic.Models.viewModel
 {
     public class LandingPage
     {
+        public User user { get; set; }
+
     }
 }
